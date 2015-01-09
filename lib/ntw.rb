fdb0d351141ff0436effdf64dd87c1e4f875fc2b
Ryan McCarthy & Grace Mekarski <student@epicodus.com>
2015-01-08 18:17:02 -0800

    Add complete ntw

--- lib/ntw.rb
+++ lib/ntw.rb
@@ -3,10 +3,36 @@ class Fixnum
     output_array = []
     ordinals = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
       "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    biggies = ["thousand", "million", "billion"]
 
     if self.<(20)
       output_array.push(ordinals[self])
+    else
+      self_split = self.to_s().split("")
+      output_array.unshift(ordinals[self_split.pop().to_i()])
+      output_array.unshift(tens[self_split.pop().to_i().-(2)])
+        if self_split.any?()
+          output_array.unshift("hundred").unshift(ordinals[self_split.pop().to_i()])
+        end
+          biggie_counter = 0
+          until self_split.empty?()
+            output_array.unshift(biggies[biggie_counter])
+            three_at_a_time = self_split.pop(3).join().to_i()
+            if three_at_a_time.<(20)
+              output_array.unshift(ordinals[three_at_a_time])
+            else
+              taat_s = three_at_a_time.to_s().split("")
+              output_array.unshift(ordinals[taat_s.pop().to_i()])
+              output_array.unshift(tens[taat_s.pop().to_i().-(2)])
+              if self_split.any?()
+                output_array.unshift("hundred").unshift(ordinals[taat_s.pop().to_i()])
+              end
+            end
+
+            biggie_counter = biggie_counter.+(1)
+          end
     end
-    output_array.join()
+    output_array.join(" ")
   end
 end
--- spec/ntw_spec.rb
+++ spec/ntw_spec.rb
@@ -1,5 +1,6 @@
 require('ntw')
 require('rspec')
+require('pry')
 
 describe ('Fixnum#ntw') do
 
@@ -12,6 +13,16 @@ describe ('Fixnum#ntw') do
     expect(15.ntw()).to(eq("fifteen"))
   end
 
+  it('works for all numbers under 100') do
+    expect(45.ntw()).to(eq("forty five"))
+  end
 
+  it('works for all numbers under 1000') do
+    expect(245.ntw()).to(eq("two hundred forty five"))
+  end
+
+  it('works for all numbers') do
+    expect(1123456789.ntw()).to(eq("one billion one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine"))
+  end
 
 end
